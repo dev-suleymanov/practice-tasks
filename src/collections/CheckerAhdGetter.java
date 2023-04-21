317a106243c4c5379e954504b09b886c1df1505d
dev-suleymanov <dev.suleymanov@gmail.com>
2023-04-21 13:17:36 +0300

    1. Проверить список и получить первый элемент.

--- src/collections/CheckerAhdGetter.java
+++ src/collections/CheckerAhdGetter.java
@@ -3,12 +3,6 @@ package collections;
 import java.util.List;
 
 public class CheckerAhdGetter {
-
-    /**
-     * Метод возвращает первый элемент из коллекции
-     * @param list - коллекция 
-     * @return первый элемент
-     */
     public static String getElement(List<String> list) {
         return list.isEmpty() ? "" : list.get(0);
     }
