7d61eae2fa9f45643473d13320e6444bce078a5b
dev-suleymanov <dev.suleymanov@gmail.com>
2023-04-22 17:35:53 +0300

    8. Удаление одного списка из другого

---
+++ src/collections/SplitterList.java
@@ -0,0 +1,35 @@
+package collections;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SplitterList {
+    public static List<String> split(List<String> left, List<String> middle, List<String> right) {
+        left.retainAll(middle);
+        left.removeAll(right);
+        return left;
+    }
+
+    public static void main(String[] args) {
+        List<String> left = new ArrayList<>();
+        left.add("one");
+        left.add("two");
+        left.add("three");
+        left.add("four");
+        left.add("five");
+        left.add("six");
+        left.add("seven");
+        left.add("eight");
+        left.add("nine");
+        left.add("ten");
+        List<String> middle = List.of(
+                "one", "two", "four", "five", "seven", "eight", "nine"
+        );
+        List<String> right = List.of(
+                "two", "five", "nine"
+        );
+        left.retainAll(right);
+        left.stream()
+                .forEach(System.out::println);
+    }
+}
