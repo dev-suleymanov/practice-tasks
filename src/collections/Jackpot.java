1130a703776f7a35ce313d75b77b1e4946961916
dev-suleymanov <dev.suleymanov@gmail.com>
2023-05-02 22:24:45 +0300

    5. Джекпот в казино

---
+++ src/collections/Jackpot.java
@@ -0,0 +1,22 @@
+package collections;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Jackpot {
+    public static boolean checkYourWin(String[] combination) {
+        Set<String> set = new HashSet<>();
+        Arrays.stream(combination)
+                .forEach(el -> set.add(el));
+        return set.size() == 1;
+    }
+
+    public static void main(String[] args) {
+        String[] first = {"@", "@", "@", "@"};
+        System.out.println(checkYourWin(first));
+        String[] second = {"&&", "&", "&&&", "&&&&"};
+        System.out.println(checkYourWin(second));
+
+    }
+}
