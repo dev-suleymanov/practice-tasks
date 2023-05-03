1edeaa8543656e6f0a85a24acdfc84744163edcd
dev-suleymanov <dev.suleymanov@gmail.com>
2023-05-03 23:14:41 +0300

    3. Получить значения отображения по ключу.

---
+++ src/collections/KeySet.java
@@ -0,0 +1,19 @@
+package collections;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class KeySet {
+    public static void main(String[] args) {
+        Map<Integer, String> map = new HashMap<>(
+                Map.of(
+                        1, "root@root",
+                        2, "local@local",
+                        3, "host@host"
+                )
+        );
+        for (Integer el : map.keySet()) {
+            System.out.println(el + " - " + map.get(el));
+        }
+    }
+}
